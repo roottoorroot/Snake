e449ad055e94880f0d7181a7af41f30348311efd
roottoorroot <ilyafulleveline@gmail.com>
2015-11-03 12:42:31 +0400

    Lesson4 List

--- Snake/ConsoleApplication1/Program.cs
+++ Snake/ConsoleApplication1/Program.cs
@@ -13,8 +13,27 @@ namespace ConsoleApplication1
             Point pointOne = new Point(1, 3, '*');
             Point pointTwo = new Point(4,5,'#');
 
-            pointOne.Drow();
-            pointTwo.Drow();
+            pointOne.Draw();
+            pointTwo.Draw();
+
+
+            List<int> numList = new List<int>();
+            numList.Add(1);
+            numList.Add(2);
+            numList.Add(3);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int num in numList)
+            {
+                Console.Write(num + " ");
+            }
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(pointOne);
+            pointList.Add(pointTwo);
 
 
 
