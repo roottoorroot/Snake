e1896290ee2451105391c39b2310a1cd5d48d43e
roottoorroot <ilyafulleveline@gmail.com>
2015-11-03 09:52:36 +0400

    lesson 2

--- Snake/ConsoleApplication1/Program.cs
+++ Snake/ConsoleApplication1/Program.cs
@@ -10,8 +10,30 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+
+
+            Drow(x1, y1, sym1);
+            Drow(x2,y2,sym2);
+
+
+
             Console.ReadKey();
+        }
+
+
+
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
         }
     }
 }
