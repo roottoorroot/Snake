32c7ed60be7a1c7152e315c56d6406b6edd36b2e
roottoorroot <ilyafulleveline@gmail.com>
2015-11-03 16:35:57 +0400

    lesson6 -More Snake: Nasledovanie

--- Snake/ConsoleApplication1/Program.cs
+++ Snake/ConsoleApplication1/Program.cs
@@ -10,23 +10,27 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Point pointOne = new Point(0, 0, '*');
+            Point pointOne = new Point(4, 5, '*');
             Point pointTwo = new Point(1,2,'#');
-            HorizontalLine gorLine = new HorizontalLine();
-            VerticalLine verLine = new VerticalLine();
+           
+            Console.SetBufferSize(80, 27);
 
 
-            pointOne.Draw();
-            pointOne.Draw(0, 20, '*');
-            pointOne.Draw(20, 20, '*');
-            pointOne.Draw(20, 0, '*');
-          
+            
 
-            gorLine.Draw(1, 20, 0, '-');
-            gorLine.Draw(1, 20, 20, '-');
-            verLine.Draw(0, 1, 20, '|');
-            verLine.Draw(20, 1, 20, '|');
+            HorizontalLine gorLineTop = new  HorizontalLine(0, 78, 0, '+');
+            HorizontalLine gorLineBottom = new HorizontalLine(0, 79, 24, '+');
+            VerticalLine verLineLeft = new VerticalLine(0, 0, 24, '+');
+            VerticalLine verLineRight = new VerticalLine(78, 0, 24, '+');
 
+            gorLineTop.Draw();
+            gorLineBottom.Draw();
+            verLineLeft.Draw();
+            verLineRight.Draw();
+
+
+            Snake snake = new Snake(pointOne, 4, Direction.right);
+            snake.Draw();
 
             Console.ReadKey();
         }
